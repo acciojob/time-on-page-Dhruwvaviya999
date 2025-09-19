04043f1e68cc3e19875f36bc7aa2fe54cd9e7f35
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-09-19 12:03:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  cont [ count, setCount ] = useState(0)
+  const interval = setInterval(() => {
+    count++;
+  }, 1000)
+ 
   return (
     <div>
         {/* Do not remove the main div */}
+        You have been on this page for {count}
     </div>
   )
 }
